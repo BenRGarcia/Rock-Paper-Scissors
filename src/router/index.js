b9f7894616d215f35e4ae8375b78e4c5dec70b6b
Ben Garcia <ben@seebenprogram.com>
2018-02-23 23:07:19 -0500

    Comment out additional router view objects

--- src/router/index.js
+++ src/router/index.js
@@ -10,6 +10,21 @@ export default new Router({
       path: '/',
       name: 'HelloWorld',
       component: HelloWorld
-    }
+    }/*,
+    {
+      path: '/',
+      name: 'HelloWorld',
+      component: HelloWorld
+    }*//*,
+    {
+      path: '/',
+      name: 'HelloWorld',
+      component: HelloWorld
+    }*//*,
+    {
+      path: '/',
+      name: 'HelloWorld',
+      component: HelloWorld
+    }*/
   ]
 })
